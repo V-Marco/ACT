cb009adb3e0f39edbccc3b17b3395b3cbeca0f85
Vladimir Omelyusik <vsomelyusik@gmail.com>
2023-08-14 16:12:42 -0500

    make plotting func plot one img at a time

--- act/analysis.py
+++ act/analysis.py
@@ -6,25 +6,23 @@ import matplotlib.pyplot as plt
 from act.optim import ACTOptimizer
 from act.metrics import correlation_score, mse_score
 
-def save_prediction_plots(target_V: torch.Tensor, simulation_constants: object, predicted_params_values: torch.Tensor, output_folder: str) -> None:
+def save_prediction_plots(target_V: torch.Tensor, amp: list, simulation_constants: object, predicted_params_values: torch.Tensor, output_folder: str) -> None:
 
-    _, ax = plt.subplots(1, 5, figsize = (20, 4))
-    for ind_amp_to_plot in range(5):
-        optim = ACTOptimizer()
-        simulated_data = optim.simulate(simulation_constants.amps[ind_amp_to_plot], simulation_constants.params, predicted_params_values.detach().numpy())
-        simulated_data = optim.resample_voltage(V = simulated_data.reshape((1, -1)), num_obs = target_V.shape[1])
+    _, ax = plt.subplots(1, 1, figsize = (10, 10))
+    optim = ACTOptimizer(simulation_constants = simulation_constants)
+    simulated_data = optim.simulate(amp, simulation_constants.params, predicted_params_values.detach().numpy())
+    simulated_data = optim.resample_voltage(V = simulated_data.reshape((1, -1)), num_obs = target_V.shape[1])
 
-        title = f"I = {simulation_constants.amps[ind_amp_to_plot]}"
-        ax[ind_amp_to_plot].plot(simulated_data.flatten(), label = "Simulated")
-        ax[ind_amp_to_plot].plot(target_V[ind_amp_to_plot], label = "Target")
-        ax[ind_amp_to_plot].set_title(title)
-        ax[ind_amp_to_plot].set_xlabel("Time (ms)")
-        ax[ind_amp_to_plot].set_ylabel("V (mV)")
-        ax[ind_amp_to_plot].legend()
-        ax[ind_amp_to_plot].grid()
+    title = f"I = {amp} nA"
+    ax.plot(simulated_data.flatten(), label = "Simulated")
+    ax.plot(target_V.flatten(), label = "Target")
+    ax.set_title(title)
+    ax.set_xlabel("Timestamp")
+    ax.set_ylabel("V (mV)")
+    ax.legend()
+    ax.grid()
 
-    fig_title = f"{np.min(simulation_constants.amps)}-{np.max(simulation_constants.amps)}_na.png"
-    plt.savefig(os.path.join(output_folder, fig_title))
+    plt.savefig(os.path.join(output_folder, f"{amp * 1000}nA.png"))
     plt.close()
 
 def save_mse_corr(target_V: torch.Tensor, simulation_constants: object, predicted_params_values: torch.Tensor, output_folder: str) -> None:
@@ -37,7 +35,7 @@ def save_mse_corr(target_V: torch.Tensor, simulation_constants: object, predicte
         sim_data = optim.simulate(amp, simulation_constants.params, predicted_params_values.detach().numpy())
         simulated_data = optim.resample_voltage(V = sim_data.reshape((1, -1)), num_obs = target_V.shape[1])
         mse = mse_score(target_V[ind].reshape(-1, 1), simulated_data.reshape(-1, 1))
-        corr = correlation_score(target_V[ind].reshape(-1, 1), simulated_data.reshape(-1, 1))
+        corr = correlation_score(target_V[ind].reshape(1, -1), simulated_data.reshape(1, -1))
 
         with open(os.path.join(output_folder, "metrics.csv"), "a") as file:
             file.write(f"{amp},{mse},{corr}\n")
