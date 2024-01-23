22b5e30b72ae2aaa81716bb4627f99e6569ebf97
Tyler Banks <tbanks@mail.missouri.edu>
2024-01-22 19:10:46 -0600

    update lab install

--- labinstall.sh
+++ labinstall.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x #echo on
+#set -x #echo on
 # Lab server setup
 
 # set your venv name
@@ -23,16 +23,27 @@ sed -i '3s/^/module purge\n/' ${ACT_VENV_LOCATION}/${ACT_VENV}/bin/activate
 sed -i '4s/^/module load mpich-x86_64-nopy\n/' ${ACT_VENV_LOCATION}/${ACT_VENV}/bin/activate
 
 # OPTIONAL! ADD THIS TO YOUR .BASHRC - Uncomment
-# echo "source ${ACT_VENV_LOCATION}/${ACT_VENV}/bin/activate" >> ${HOME}/.bashrc
+#echo "source ${ACT_VENV_LOCATION}/${ACT_VENV}/bin/activate" >> ${HOME}/.bashrc
 # or just run `source act-venv/bin/activate` everytime you login
 
 # activate the environment without logging out 
 source ${ACT_VENV_LOCATION}/${ACT_VENV}/bin/activate
 
-# clone the repo
-git clone https://github.com/V-Marco/ACT
-
-# install dependancies
-cd ACT
-pip install -e .
+# return to act dir and install dependancies
+cd - 
+pip install --upgrade pip
 pip install mpi4py bmtk
+pip install -e .
+
+
+# info
+echo "\n"
+echo "*****************************************************"
+echo "*****************************************************"
+echo 
+echo "ACTIVATE YOUR ENVIRONMENT BY RUNNING THE FOLLOWING"
+echo "source ${ACT_VENV_LOCATION}/${ACT_VENV}/bin/activate"
+echo
+echo "*****************************************************"
+echo "*****************************************************"
+echo
