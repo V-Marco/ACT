7fbedac9d9270f5577e0cd93f29fe82e595deb99
mwstroud <stroudm777@gmail.com>
2024-02-04 21:23:53 -0600

    Logging update

--- simulation/generate_traces.py
+++ simulation/generate_traces.py
@@ -6,6 +6,8 @@ from act.utils import build_parametric_network, generate_parametric_traces
 
 if __name__ == "__main__":
     if "build" in sys.argv:
+        #print("Building Network")
         build_parametric_network(selected_config)
     else:
+        #print("Generating Traces")
         generate_parametric_traces(selected_config)
