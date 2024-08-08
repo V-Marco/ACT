224baa60f96c1e316f27d9ad1731eb9e10873825
mwstroud <stroudm777@gmail.com>
2024-08-07 21:09:18 -0500

    mae surface plot of spiker train cell vs target cell data.

--- act/ACTPlot.py
+++ act/ACTPlot.py
@@ -1,6 +1,9 @@
 import os
 import numpy as np
 from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from act.DataProcessor import DataProcessor
+from act.Metrics import Metrics
 
 def create_overlapped_v_plot(x, y1, y2, module_foldername, title, filename):
     plt.figure(figsize=(8, 6))
@@ -50,3 +53,71 @@ def plot_fi_comparison(fi_data_filepath, amps):
     plt.legend()
     plt.savefig(results_folder + "FI_Curve_Comparison.png")
     plt.close()
+    
+def plot_training_mae_surface_spiker_cell(target_data_filepath, training_data_filepath, delay, dt):
+    # load target data
+    target_dataset = np.load(target_data_filepath)
+    
+    target_V = target_dataset[:,:,0]
+    target_I = target_dataset[:,:,1]
+    
+    # load training data
+    train_dataset = np.load(training_data_filepath)
+    train_V = train_dataset[:,:,0]
+    train_I = train_dataset[:,:,1]
+    train_g = train_dataset[:,:,2]
+    
+    index_where_inj_occurs = int(delay / dt + 1)
+    target_I_sample = target_I[0]
+    #print(target_I_sample[index_where_inj_occurs])
+    
+    metrics = Metrics()
+    maes = []
+
+    # Calculate MAE for matching current injection traces
+    for target_idx, target_V_sample in enumerate(target_V):
+        target_I_sample = target_I[target_idx]
+        
+
+        matching_training_indices = np.where(train_I[:,index_where_inj_occurs] == target_I_sample[index_where_inj_occurs])[0]
+        print(f"found indices: {len(matching_training_indices)}")
+        if len(matching_training_indices) == 0:
+            continue  
+
+        for train_idx in matching_training_indices:
+            train_V_sample = train_V[train_idx]
+
+            mae = metrics.mae_score(target_V_sample, train_V_sample)
+            conductance_values = train_g[train_idx]
+            
+            #print((conductance_values[0], conductance_values[1], mae))
+            maes.append((conductance_values[0], conductance_values[1], mae))
+
+    # Convert results to numpy array for plotting
+    maes = np.array(maes)
+    #print(maes.shape)
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    # Scatter plot
+    #print(maes[:, 0])
+    scatter = ax.scatter(maes[:, 0], maes[:, 1], maes[:, 2], c=maes[:, 2], cmap='viridis', marker='o')
+
+    ax.set_xlabel("gNA_bar")
+    ax.set_ylabel("gK_bar")
+    
+    cbar = fig.colorbar(scatter, shrink=0.5, aspect=5)  # Add a color bar to help interpret the colors
+    cbar.set_label('MAE')
+
+    plt.title('MAE Plot')
+    
+    ax.view_init(elev=20, azim=135)
+    
+    fig.savefig('MAE_Surface.png')
+    plt.show()
+    
+    
+    
+    
+    
