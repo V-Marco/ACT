1d89acd84c1cc7dbddc9903e9039cbb6caebe157
mwstroud0 <mwstroud0@gmail.com>
2025-06-27 04:47:14 +0000

    LAA original pipeline latest

--- data/L5/cell_builder.py
+++ data/L5/cell_builder.py
@@ -3,12 +3,12 @@
 def L5cellbuilder():
     from neuron import h
     # Load biophysics
-    h.load_file("/Users/vladimiromelyusik/ACT/data/L5/orig/L5PCbiophys3ActiveBasal.hoc")
+    h.load_file("orig/L5PCbiophys3ActiveBasal.hoc")
 
     # Load morphology
     h.load_file("import3d.hoc")
 
     # Load template
-    h.load_file("/Users/vladimiromelyusik/ACT/data/L5/orig/L5PCtemplate.hoc")
+    h.load_file("orig/L5PCtemplate.hoc")
 
-    return getattr(h, "L5PCtemplate")("/Users/vladimiromelyusik/ACT/data/L5/orig/cell1.asc")
+    return getattr(h, "L5PCtemplate")("orig/cell1.asc")
